2a5986ba93206b950bdc56e27490a659e1e12c18
sellamikhouloud <154612472+sellamikhouloud@users.noreply.github.com>
2026-01-04 22:22:21 +0000

    Update filetools.py

--- src/tools/filetools.py
+++ src/tools/filetools.py
@@ -5,14 +5,15 @@ import time
 
 
 def read_file(path):
-    if not path.startswith("sandbox"):
-        raise PermissionError("Accès interdit hors sandbox")
+if not path.startswith("sandbox"):
+    raise PermissionError("Accès interdit hors sandbox")
     with open(path, "r", encoding="utf-8") as f:
         return f.read()
 
 
 def write_file(path, content):
-    if not path.startswith("sandbox"):
+     if not path.startswith("sandbox"):
         raise PermissionError("Accès interdit hors sandbox")
     with open(path, "w", encoding="utf-8") as f:
+
         f.write(content)
