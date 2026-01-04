8e4e80b7e38fae354e79e9c5c6beb3ca576ca97b
sellamikhouloud <154612472+sellamikhouloud@users.noreply.github.com>
2026-01-04 21:49:56 +0000

    Add files via upload

---
+++ src/tools/pytesttools.py
@@ -0,0 +1,13 @@
+import subprocess
+
+def runpytest(target_dir):
+    result = subprocess.run(
+        ["pytest", target_dir],
+        capture_output=True,
+        text=True
+    )
+    return {
+        "success": result.returncode == 0, #si sup a 0 donc ya des test qui echoue 
+        "stdout": result.stdout,# imprime tout ce que ce outil donne les resultats des tests 
+        "stderr": result.stderr #les erreurs taper par l'outil 
+    }
