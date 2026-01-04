5368437fa577023a74c5152413af7b7972c9d817
sellamikhouloud <154612472+sellamikhouloud@users.noreply.github.com>
2026-01-04 22:23:09 +0000

    Update pytesttools.py

--- src/tools/pytesttools.py
+++ src/tools/pytesttools.py
@@ -1,13 +1,14 @@
 import subprocess
 
 def runpytest(target_dir):
-    result = subprocess.run(
-        ["pytest", target_dir],
-        capture_output=True,
-        text=True
+  result = subprocess.run(
+         ["pytest", target_dir],
+         capture_output=True,
+         text=True
     )
     return {
-        "success": result.returncode == 0, #si sup a 0 donc ya des test qui echoue 
-        "stdout": result.stdout,# imprime tout ce que ce outil donne les resultats des tests 
+     "success": result.returncode == 0, #si sup a 0 donc ya des test qui echoue 
+         "stdout": result.stdout,# imprime tout ce que ce outil donne les resultats des tests 
         "stderr": result.stderr #les erreurs taper par l'outil 
     }
+
