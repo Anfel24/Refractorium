c6394537e799f5996c69b2cf5ab3f6ad44e05e09
sellamikhouloud <154612472+sellamikhouloud@users.noreply.github.com>
2026-01-04 22:22:47 +0000

    Update pylinttools.py

--- src/tools/pylinttools.py
+++ src/tools/pylinttools.py
@@ -1,14 +1,15 @@
 import subprocess
 
 def runpylint(target_dir):
-    result = subprocess.run(
-        ["pylint", target_dir],
+ result = subprocess.run(
+     ["pylint", target_dir],
         capture_output=True,
-        text=True
+      text=True
     )
     
     return {
-        "stdout": result.stdout,
-        "stderr": result.stderr,
-        "returncode": result.returncode #code indique est ce que le code  la gravite de code 
+         "stdout": result.stdout,
+         "stderr": result.stderr,
+         "returncode": result.returncode #code indique est ce que le code  la gravite de code 
     }
+
