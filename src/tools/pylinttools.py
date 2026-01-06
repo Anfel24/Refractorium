49a74863fa78ea4261aebf3cce6386857039bca3
sellamikhouloud <154612472+sellamikhouloud@users.noreply.github.com>
2026-01-06 17:07:02 +0000

    Update pylinttools.py

--- src/tools/pylinttools.py
+++ src/tools/pylinttools.py
@@ -1,15 +1,27 @@
+
+
 import subprocess
 
-def runpylint(target_dir):
- result = subprocess.run(
-     ["pylint", target_dir],
+def runpytest(target_dir: str):
+    result = subprocess.run(
+        ["pytest", target_dir],
         capture_output=True,
-      text=True
+        text=True
     )
-    
-    return {
-         "stdout": result.stdout,
-         "stderr": result.stderr,
-         "returncode": result.returncode #code indique est ce que le code  la gravite de code 
-    }
 
+    success = (result.returncode == 0)
+
+    logs = []
+    logs.append("===== rapport de pytest =====")
+
+    if result.stdout.strip():
+        logs.append("---- sortie standard ----")
+        logs.append(result.stdout)
+
+    if result.stderr.strip():
+        logs.append("---- erreurs ----")
+        logs.append(result.stderr)
+
+    logs.append(f"---- le code: {result.returncode} ----") #code indique est ce que le code  la gravite de code 
+
+    return success, "\n".join(logs)
