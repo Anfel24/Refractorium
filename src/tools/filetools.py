cb76d3ccb58ec5a862a1b1b18ec62831d605aee1
sellamikhouloud <154612472+sellamikhouloud@users.noreply.github.com>
2026-01-06 17:04:56 +0000

    Update filetools.py

--- src/tools/filetools.py
+++ src/tools/filetools.py
@@ -17,3 +17,27 @@ def write_file(path, content):
     with open(path, "w", encoding="utf-8") as f:
 
         f.write(content)
+
+ 
+
+def save_files_to_disk(target_dir: str, files_content: dict):
+  
+   # Écrit les fichiers du projet sur le disque.
+
+   
+ 
+
+  
+    os.makedirs(target_dir, exist_ok=True)
+
+    # Parcourir tous les fichiers a ecrire
+    for file_path, content in files_content.items():
+        # Construire le chemin complet du fichier
+        full_path = os.path.join(target_dir, file_path)
+
+      
+        os.makedirs(os.path.dirname(full_path), exist_ok=True)
+
+        # Écrire le contenu dans le fichier
+        with open(full_path, "w", encoding="utf-8") as f:
+            f.write(content)
